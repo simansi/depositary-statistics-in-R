95b6be6a770e9499abb600cd7411988a2b6fd0e4
simansi <111124604+simansi@users.noreply.github.com>
2022-08-12 18:06:48 +0530

    Add files via upload

---
+++ Descriptive_Vis.R
@@ -0,0 +1,16 @@
+
+#Descriptive statistics
+#1
+summary(quakes[,c(4,5)])
+
+quakes$depth
+quakes$mag
+quakes$stations
+
+#1
+summary(quakes$stations)
+########Summary of the data########
+
+summary(quakes)
+summary(quakes$stations)
+
